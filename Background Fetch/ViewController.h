026b40dcb7664e30263f501a23c92913634446c7
Bilal <bilal.arslan1993@hotmail.com>
2014-07-17 14:09:54 +0300

    Created XMLParser class for RSS

--- Background Fetch/ViewController.h
+++ Background Fetch/ViewController.h
@@ -1,4 +1,10 @@
-
+//
+//  ViewController.h
+//  Background Fetch
+//
+//  Created by Bilal ARSLAN on 17/07/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 
